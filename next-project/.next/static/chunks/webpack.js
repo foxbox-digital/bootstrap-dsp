3aab9419e9fb54adda415c6e6a5bdb50668e2a18
marcomaza92 <marco.maza@foxbox.com>
2020-10-07 18:50:28 -0300

    fix(projects): update next build generics

--- next-project/.next/static/chunks/webpack.js
+++ next-project/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "470183d54f953820bd3d";
+/******/ 	var hotCurrentHash = "781c3e5e69f06a498dcf";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
